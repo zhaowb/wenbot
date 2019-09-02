d8bd92afab695ea50f61a016bc22338a2f07ab92
Wenbo Zhao <zhaowb@gmail.com>
2019-09-03 07:29:50 +1200

    update version and release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wenbot",
-    version="0.0.1",
+    version="0.0.2",
     author="Wenbo Zhao",
     author_email="zhaowb@gmail.com",
     description="Simple bot wrapper of selenium basic functions",
